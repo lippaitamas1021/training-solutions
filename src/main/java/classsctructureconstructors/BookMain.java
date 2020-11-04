3b0bea88c841706eed1a14b44bc85e9ccd28773d
lippaitamas1021 <lippaitamas1021@gmail.com>
2020-11-04 18:11:31 +0100

    BookMain commit

---
+++ src/main/java/classsctructureconstructors/BookMain.java
@@ -0,0 +1,15 @@
+package classsctructureconstructors;
+
+public class BookMain {
+
+    public static void main(String[] args) {
+
+        Book book = new Book("Reiter István", "C# Programozás Lépésről-lépésre");
+
+        book.register("321");
+
+        System.out.println("Szerző: " + book.getAuthor());
+        System.out.println("Cím: " + book.getTitle());
+        System.out.println("Regisztrációs szám: " + book.getRegNumber());
+        }
+}
