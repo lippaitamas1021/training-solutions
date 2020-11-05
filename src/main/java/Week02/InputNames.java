010455680bc3006a5e5aa88919d5710c5990f72a
lippaitamas1021 <lippaitamas1021@gmail.com>
2020-11-05 10:30:32 +0100

    InputNames commit

---
+++ src/main/java/Week02/InputNames.java
@@ -0,0 +1,19 @@
+package Week02;
+
+import java.util.Scanner;
+
+public class InputNames {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] arrayOfNames = new int[5];
+
+        for (int i = 0; i < arrayOfNames.length; i++);
+        {
+        }
+
+        System.out.println(arrayOfNames[i]);
+    }
+}
