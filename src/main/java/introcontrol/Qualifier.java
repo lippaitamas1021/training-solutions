c8b784748d0ff359056fdf622f40bd9ec97864e7
lippaitamas1021 <lippaitamas1021@gmail.com>
2020-11-11 16:28:25 +0100

    Qualifier commit

---
+++ src/main/java/introcontrol/Qualifier.java
@@ -0,0 +1,19 @@
+package introcontrol;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Qualifier {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Kérem, adjon meg egy számot1 és 1000 között:");
+        int Num1 = scanner.nextInt();
+
+        if (Num1 > 100) {
+            System.out.println("Az Ön által megadott szám nagyobb, mint 100.");
+        }  else {
+            System.out.println("Az Ön által megadott szám kisebb, mint 100.");
+        }
+    }
+}
