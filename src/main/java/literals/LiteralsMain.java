c048b59d878ad87b744be109ec9c80d99a445c13
lippaitamas1021 <lippaitamas1021@gmail.com>
2020-11-13 10:47:51 +0100

    LiteralsMain commit

---
+++ src/main/java/literals/LiteralsMain.java
@@ -0,0 +1,26 @@
+package literals;
+
+public class LiteralsMain {
+
+    public static void main(String[] args) {
+        System.out.println("1" + "  " + "2");
+
+        double quotient = 3.0/4;
+        System.out.println(quotient);
+
+        long big = 3_244_444_444L;
+        System.out.println(big);
+
+        String s = "árvíztűrőtükörfúrógép";
+        System.out.println(s);
+
+        String word = "title".toUpperCase();
+        System.out.println(word);
+
+        System.out.println(Integer.toBinaryString(1));
+        System.out.println(Integer.toBinaryString(-2));
+    }
+
+
+
+}
