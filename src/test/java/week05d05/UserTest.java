a1ea2d38b62c0d92e1ea9632e3363687ee2d3827
lippaitamas1021 <lippaitamas1021@gmail.com>
2020-12-01 09:34:41 +0100

    week05d05 commit #2

--- src/test/java/week05d05/UserTest.java
+++ src/test/java/week05d05/UserTest.java
@@ -3,13 +3,22 @@ package week05d05;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class UserTest {
 
     @Test
     public void getFullNameTest() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            throw new IllegalArgumentException(("nem megfelelő"));
+        assertThrows(IllegalArgumentException.class, () -> {
+            new User("Tamás", "Lippai", "lt.gmail.com");
         });
+    }
 
+    @Test
+    public void testUserNoAt() throws IllegalArgumentException {
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->
+                new User("Tamás", "Lippai", "lt.gmail.com"));
+        assertEquals("Hibás e-mail cím! \'@\' vagy \'.\' hiányzik!", exception.getMessage());
     }
 }
