8092fed735cb3fb6bc7a93ffd5e1fb69410d7710
lippaitamas1021 <lippaitamas1021@gmail.com>
2020-12-28 08:54:28 +0100

    Car commit

---
+++ src/main/java/inheritanceconstructor/car/Jeep.java
@@ -0,0 +1,41 @@
+package inheritanceconstructor.car;
+
+public class Jeep extends Car {
+
+    private double extraCapacity;
+    private double extraFuel;
+
+    public Jeep(double fuelRate, double fuel, double tankCapacity, double extraCapacity, double extraFuel) {
+        super(fuelRate, fuel, tankCapacity);
+        if (extraCapacity < extraFuel) {
+            throw new IllegalArgumentException("Auxillary capacity is less than extra fuel!");
+        }
+        this.extraCapacity = extraCapacity;
+        this.extraFuel = extraFuel;
+    }
+
+    public double getExtraFuel() {
+        return extraFuel;
+    }
+
+    private boolean isEnoughFuel(int distance) {
+        return (super.getFuel() + extraFuel - distance * super.getFuelRate() / 100) >= 0.0;
+    }
+
+    public void drive(int km) {
+        if (!isEnoughFuel(km)) {
+            throw new IllegalArgumentException("Not enough fuel available!");
+        }
+        double usedFuel = km * super.getFuelRate() / 100;
+        extraFuel -= usedFuel;
+        if (extraFuel < 0) {
+            modifyFuelAmount(extraFuel);
+            extraFuel = 0;
+        }
+    }
+
+    public double calculateRefillAmount() {
+        return super.getTankCapacity() + extraCapacity - (super.getFuel() + extraFuel);
+    }
+
+}
