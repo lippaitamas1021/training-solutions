3ee95c90f48784bd111036664a74edc0c076343c
lippaitamas1021 <lippaitamas1021@gmail.com>
2020-12-28 09:38:55 +0100

    Room, ClassRoom, Course commit

---
+++ src/test/java/inheritanceconstructor/classroom/RoomTest.java
@@ -0,0 +1,16 @@
+package inheritanceconstructor.classroom;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RoomTest {
+
+    @Test
+    public void constructorTest() {
+        //Given
+        Room room = new Room("I. em. 12.", 20);
+        //Then
+        assertEquals("I. em. 12.", room.getLocation());
+        assertEquals(20, room.getCapacity());
+    }
+}
