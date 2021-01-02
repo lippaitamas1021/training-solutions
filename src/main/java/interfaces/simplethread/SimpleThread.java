a040bb3f28043c79c062f4665838256b8603be0b
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-01-02 15:09:44 +0100

    interfaces commit

---
+++ src/main/java/interfaces/simplethread/SimpleThread.java
@@ -0,0 +1,35 @@
+package interfaces.simplethread;
+
+import com.sun.source.tree.BreakTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleThread implements Runnable{
+
+    @Override
+    public void run() {
+        while(!tasks.isEmpty()) {
+            nextStep();
+        }
+    }
+
+    private List<String> tasks;
+
+    public List<String> getTasks() {
+        return tasks;
+    }
+
+    public SimpleThread(List<String> tasks) {
+        this.tasks = tasks;
+    }
+
+    private void nextStep() {
+        if (tasks != null && !tasks.isEmpty()) {
+            tasks.remove(tasks.size() - 1);
+        }
+    }
+
+
+
+}
