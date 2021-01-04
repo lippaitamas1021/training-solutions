90afcc444256d288485014804ab0ea9b95bde5e6
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-01-04 15:58:37 +0100

    virtualmethod.vehicle commit

---
+++ src/main/java/virtualmethod/vehicle/Van.java
@@ -0,0 +1,29 @@
+package virtualmethod.vehicle;
+
+public class Van extends Car {
+
+    private int cargoWeight;
+
+    public Van(int numberPassenger, int vehicleWeight, int cargoWeight) {
+        super(vehicleWeight, numberPassenger);
+        this.cargoWeight = cargoWeight;
+    }
+
+    public int getCargoWeight() {
+        return cargoWeight;
+    }
+
+    @Override
+    public int getGrossLoad() {
+        return super.getGrossLoad() + cargoWeight;
+    }
+
+    @Override
+    public String toString() {
+        return "Van{" +
+                "cargoWeight=" + cargoWeight +
+                ", numberOfPassenger=" + numberPassenger +
+                ", vehicleWeight=" + getVehicleWeight() +
+                '}';
+    }
+}
