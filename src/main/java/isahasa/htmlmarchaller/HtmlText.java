9115125c3c4107f9ffe91ce661262c29bdaad46f
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-01-04 17:15:04 +0100

    isahasa.htmlmarchaller commit

---
+++ src/main/java/isahasa/htmlmarchaller/HtmlText.java
@@ -0,0 +1,17 @@
+package isahasa.htmlmarchaller;
+
+public class HtmlText implements TextSource {
+
+    private final String plainText;
+
+    public HtmlText(String plainText) {
+        this.plainText = plainText;
+    }
+
+    @Override
+    public String getPlainText() {
+        return plainText;
+    }
+
+
+}
