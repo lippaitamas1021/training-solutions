dae9b8bfeccdddbd008b18bfbb49fd99afefecf4
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-01-04 18:07:46 +0100

    isahasa.fleet commit

---
+++ src/main/java/isahasa/fleet/Liner.java
@@ -0,0 +1,18 @@
+package isahasa.fleet;
+
+public class Liner implements Ship, CanCarryPassengers {
+
+    private final CanCarryPassengers canCarryPassengers;
+
+    public Liner(int maxPassengers) {
+        canCarryPassengers = new CanCarryPassengersBehaviour(maxPassengers);
+    }
+
+    public int getPassengers() {
+        return canCarryPassengers.getPassengers();
+    }
+
+    public int loadPassenger(int passengers){
+        return canCarryPassengers.loadPassenger(passengers);
+    }
+}
