65ec60ee96fb885301b5dfa1eb9d566aa96b12b7
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-01-09 08:48:40 +0100

    exam02 ArraySelector commit #3

--- src/main/java/exam02/ArraySelector.java
+++ src/main/java/exam02/ArraySelector.java
@@ -2,27 +2,24 @@ package exam02;
 
 public class ArraySelector {
 
-    public boolean isEmpty(int[] numbers) {
-        return numbers.length == 0;
-    }
-
     public String selectEvens(int[] numbers) {
+
         StringBuilder sb = new StringBuilder();
-        if (isEmpty(numbers)) {
-            return "";
-        }
-        int [] evenNumbers = new int[numbers.length];
-        int db = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] % 2 != 0) {
-                evenNumbers[db] = numbers[i];
-                sb.append(numbers[i]);
+
+        if (0 < numbers.length) {
+            sb.append("[");
+        } else {
+            return ""; }
+
+            for (int i = 0; i < numbers.length; i++) {
+                if (i % 2 == 0) {
+                    sb.append(numbers[i]).append(", ");
+                }
             }
-            }
-        String[] s = sb.toString().split("");
-        return "[" + sb.toString() + "]";
-            }
-        }
+        sb.delete(sb.length() - 2, sb.length());
+        return sb.append("]").toString();
+    }
+}
 
 
 
--- src/test/java/exam02/ArraySelectorTest.java
+++ src/test/java/exam02/ArraySelectorTest.java
@@ -10,9 +10,9 @@ public class ArraySelectorTest {
         assertEquals("", new ArraySelector().selectEvens(new int[]{}));
         assertEquals("[1]", new ArraySelector().selectEvens(new int[]{1}));
         assertEquals("[1]", new ArraySelector().selectEvens(new int[]{1, 2}));
-        //assertEquals("[1, 3]", new ArraySelector().selectEvens(new int[]{1, 2, 3}));
-        //assertEquals("[1, 3]", new ArraySelector().selectEvens(new int[]{1, 2, 3, 4}));
-        //assertEquals("[1, 3, 5]", new ArraySelector().selectEvens(new int[]{1, 2, 3, 4, 5}));
+        assertEquals("[1, 3]", new ArraySelector().selectEvens(new int[]{1, 2, 3}));
+        assertEquals("[1, 3]", new ArraySelector().selectEvens(new int[]{1, 2, 3, 4}));
+        assertEquals("[1, 3, 5]", new ArraySelector().selectEvens(new int[]{1, 2, 3, 4, 5}));
 
     }
 }
