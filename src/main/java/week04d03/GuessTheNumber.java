d4d9d4d7a637f5d86315ba5b6b9c427599edbad8
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-01-11 10:44:10 +0100

    week04d03 commit #2

--- src/main/java/week04d03/GuessTheNumber.java
+++ src/main/java/week04d03/GuessTheNumber.java
@@ -6,22 +6,25 @@ import java.util.Scanner;
 public class GuessTheNumber {
 
     public static void main(String[] args) {
-
+        Random random = new Random();
+        int number1 = random.nextInt(101);
         Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < 3; i++) {
-            Random random = new Random();
-            int number1 = random.nextInt(101);
+        for (int i = 0; i < 7; i++) {
             System.out.println("Kérem, adjon meg egy számot 1 és 100 között:");
             int number2 = scanner.nextInt();
             scanner.nextLine();
-            if (number1 < number2) {
+            if (number1 == number2) {
+                System.out.println("Ön eltaláta az általam véletlenszerűen választott számot!");
                 System.out.println(number1);
+                return;
+            } if (number1 < number2) {
                 System.out.println("Az Ön által választott szám nagyobb!");
             } else {
-                System.out.println(number1);
                 System.out.println("Az általam választott szám nagyobb!");
             }
+            }
+        System.out.println("Az általam véletlenszerűen választott szám: " + number1);
         }
     }
-}
+
