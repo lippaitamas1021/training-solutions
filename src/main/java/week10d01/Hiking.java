8786589c3b587a4b811b95cc540474831f8774d2
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-01-11 16:37:00 +0100

    week10d01 commit #2

--- src/main/java/week10d01/Hiking.java
+++ src/main/java/week10d01/Hiking.java
@@ -4,6 +4,9 @@ import java.util.List;
 public class Hiking {
 
     public double getPlusElevation(List<Double> heights) {
+        if (heights == null) {
+            throw new IllegalArgumentException("List is empty");
+        }
         double sum = 0;
         for (int i = 1; i < heights.size(); i++) {
             if (heights.get(i-1) < heights.get(i)) {
--- src/test/java/week10d01/HikingTest.java
+++ src/test/java/week10d01/HikingTest.java
@@ -1,6 +1,8 @@
 package week10d01;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,8 +13,15 @@ class HikingTest {
         Hiking hiking = new Hiking();
         double result;
         result = hiking.getPlusElevation(List.of(10d,20d, 15d, 18d));
-
         assertEquals(13, result);
+
+        assertEquals(13.0, new Hiking().getPlusElevation(List.of(10.0, 20.0, 15.0, 18.0)));
+
+        assertEquals(0.0, new Hiking().getPlusElevation(List.of(10.0)));
+
+        assertEquals(0.0, new Hiking().getPlusElevation(List.of()));
+
+        assertThrows(IllegalArgumentException.class, () -> new Hiking().getPlusElevation(null));
 
     }
 }
