61c8814660379a5ac69f897fafda62fd31179219
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-01-12 08:15:41 +0100

    week10d02 commit

---
+++ src/main/java/week10d02/MaxTravel.java
@@ -0,0 +1,34 @@
+package week10d02;
+;
+import java.util.List;
+
+public class MaxTravel {
+
+    public int getMaxIndex(List<Integer> numbers) {
+        int[] counters = count(numbers);
+        return max(counters);
+    }
+
+    private int[] count(List<Integer> numbers) {
+        int[] counters = new int[30];
+        for (int i : numbers) {
+            counters[i]++;
+        }
+        return counters;
+    }
+
+    private int max(int[] counters) {
+        int index = 0;
+        for (int i = 1; i < counters.length; i++) {
+            if (counters[index] < counters[i]) {
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        int max = new MaxTravel().getMaxIndex(List.of(12, 12, 0, 3, 4, 4, 12));
+        System.out.println(max);
+    }
+}
