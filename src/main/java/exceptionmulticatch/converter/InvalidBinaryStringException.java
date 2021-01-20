ba3898ed541dd8275afaad4c1a94b834aef68664
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-01-20 14:32:33 +0100

    exceptionmulticatch.converter commit

---
+++ src/main/java/exceptionmulticatch/converter/InvalidBinaryStringException.java
@@ -0,0 +1,26 @@
+package exceptionmulticatch.converter;
+
+public class InvalidBinaryStringException extends RuntimeException {
+
+    public InvalidBinaryStringException() {
+    }
+
+    public InvalidBinaryStringException(String message) {
+        super(message);
+    }
+
+    public InvalidBinaryStringException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidBinaryStringException(Throwable cause) {
+        super(cause);
+    }
+
+    public InvalidBinaryStringException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public static void error(String message, RuntimeException ex) {
+    }
+}
