7954d6200a97649ded2d4b6fce5bd4c56e487acc
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-01-20 16:34:41 +0100

    ioreadstring transaction commit

---
+++ src/main/java/transaction/TransactionManager.java
@@ -0,0 +1,48 @@
+package transaction;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TransactionManager {
+
+    private List<BankAccount> accountList = new ArrayList<>();
+
+    public void uploadListFromFile(String path){
+        Path accounts = Path.of(path);
+        try {
+            List<String> accountsFromFile = Files.readAllLines(accounts);
+            for(String account : accountsFromFile){
+                String[] temp = account.split(";");
+                BankAccount bankAccount = new BankAccount(temp[0],temp[1],Integer.parseInt(temp[2]));
+                accountList.add(bankAccount);
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can't open file",ioe);
+        }
+    }
+
+    public void makeTransactions(String path){
+        Path transactions = Path.of(path);
+        try {
+            List<String> transactionsFromFile = Files.readAllLines(transactions);
+
+            for(String s : transactionsFromFile){
+                String[] temp = s.split(";");
+                for(BankAccount bankAccount : accountList){
+                    if(temp[0].equals(bankAccount.getAccountNumber())){
+                        bankAccount.setBalance(Double.parseDouble(temp[1]));
+                    }
+                }
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not find the file",ioe);
+        }
+    }
+
+    public List<BankAccount> getAccountList() {
+        return accountList;
+    }
+}
---
+++ src/test/java/transaction/TransactionManagerTest.java
@@ -0,0 +1,26 @@
+package transaction;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TransactionManagerTest {
+
+    private TransactionManager tm = new TransactionManager();
+
+    @Test
+    void testUploadAccounts() {
+        assertEquals(0, tm.getAccountList().size());
+        tm.uploadListFromFile("accounts.txt");
+        assertEquals(5, tm.getAccountList().size());
+        assertEquals("Jack Doe", tm.getAccountList().get(3).getName());
+        assertEquals(234050200, tm.getAccountList().get(1).getBalance(), 0.0000001);
+    }
+
+    @Test
+    void makeTransactionsTest() {
+        tm.uploadListFromFile("accounts.txt");
+        tm.makeTransactions("transactions.txt");
+        assertEquals(1201000, tm.getAccountList().get(0).getBalance(), 0.0000001);
+        assertEquals(721220, tm.getAccountList().get(3).getBalance(), 0.00001);
+    }
+}
