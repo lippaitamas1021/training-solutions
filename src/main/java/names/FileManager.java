99554b60d91486e82ea1ccbabbfa5edd3e312145
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-01-20 15:55:23 +0100

    ioreadstring names commit

---
+++ src/main/java/names/FileManager.java
@@ -0,0 +1,37 @@
+package names;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileManager {
+
+    private Path path;
+    private List<Human> humans = new ArrayList<>();
+
+
+    public FileManager(String fileRealtivePath) {
+        this.path =  Path.of(fileRealtivePath);
+    }
+
+    public void readFromFile() throws IOException {
+            List<String> fileInString = Files.readAllLines(path);
+            for (String s : fileInString) {
+                String firstAndLast[] = s.split(" ");
+                Human h = new Human(firstAndLast[0], firstAndLast[1]);
+                humans.add(h);
+            }
+    }
+
+
+    public List<Human> getHumans() {
+        return new ArrayList<>(humans);
+    }
+
+
+    public Path getPath() {
+        return path;
+    }
+}
---
+++ src/test/java/names/FileManagerTest.java
@@ -0,0 +1,26 @@
+package names;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FileManagerTest {
+
+    private FileManager fm = new FileManager("names.txt");
+
+    @Test
+    public void createClassTest() {
+        assertEquals("names.txt", fm.getPath().getFileName().toString());
+        assertEquals(0, fm.getHumans().size());
+    }
+
+
+    @Test
+    public void readFromFileTest() throws IOException {
+        fm.readFromFile();
+        assertEquals(5, fm.getHumans().size());
+        assertEquals("Jane", fm.getHumans().get(1).getFirstName());
+    }
+}
