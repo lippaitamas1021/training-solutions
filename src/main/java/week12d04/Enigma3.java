60f6bc72872e5f0c917b49155031704a84a66b57
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-01-25 15:29:39 +0100

    week12d04 commit #3

--- src/main/java/week12d04/Enigma3.java
+++ src/main/java/week12d04/Enigma3.java
@@ -14,7 +14,7 @@ public class Enigma3 {
             int size;                                           // általában 64 kbyte-os pufferrel dolgozunk
             while (0 < (size = is.read(buffer))) {
                 System.out.println(size);
-                for (int i = 0; i < size; i++) {
+                for (int i = 0; i < size; i++) {                // csak a size méretéig szabad menni, nem a buffer végéig
                     char c = (char) (buffer[i] + 10);
                     sb.append(c);
                 }
---
+++ src/test/java/week12d04/DecoderTest.java
@@ -0,0 +1,20 @@
+/*package week12d04;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DecoderTest {
+
+    @Test
+    public void decodeTest() {
+        Decoder decoder = new Decoder();
+
+        Exception exception = assertThrows(IllegalStateException.class, () -> decoder.decode();
+        assertEquals("Can not decode file", exception.getMessage());
+    }
+}
+
+ */
