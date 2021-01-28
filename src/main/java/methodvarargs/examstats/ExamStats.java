6203797e716ff63d66badb46d1592e09d5d399ed
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-01-28 09:26:48 +0100

    methodvarargs.examstats commit

---
+++ src/main/java/methodvarargs/examstats/ExamStats.java
@@ -0,0 +1,35 @@
+package methodvarargs.examstats;
+
+public class ExamStats {
+
+    private int maxPoints;
+
+    public ExamStats(int masPoints) {
+        this.maxPoints = masPoints;
+    }
+
+    public int getNumberOfTopGrades(int limitInPercent, int... results) {
+        if (results == null || results.length == 0) {
+            throw new IllegalArgumentException("Number of results must not be empty!");
+        }
+        int count = 0;
+        for (Integer result : results) {
+            if (result > limitInPercent) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public boolean hasAnyFailed(int limitInPercent, int... results) {
+        if (results == null || results.length == 0) {
+            throw new IllegalArgumentException("Number of results must not be empty!");
+        }
+            for (Integer result : results) {
+                if (result < limitInPercent) {
+                    return true;
+                }
+            }
+        return false;
+        }
+    }
