c327ab388e6fa9633d0bfdd4bfc2b86c827b2f6f
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-02-10 10:33:33 +0100

    ioconvert.products commit

---
+++ src/main/java/ioconvert/products/ProductWriter.java
@@ -0,0 +1,18 @@
+package ioconvert.products;
+
+import java.io.*;
+import java.util.List;
+
+public class ProductWriter {
+
+    public void saveProduct(OutputStream os, List<Product> products) {
+        try (PrintStream pw = new PrintStream(os)) {
+            for (Product product : products) {
+                pw.print(product.getName());
+                pw.print(";");
+                pw.print(product.getPrice());
+                pw.println();
+            }
+        }
+    }
+}
