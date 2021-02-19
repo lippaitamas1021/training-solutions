c94934755f56af546b193a77a7503934d6c72a44
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-02-19 10:52:31 +0100

    week08d02 commit #2

--- src/main/java/week08d02/Lottery.java
+++ src/main/java/week08d02/Lottery.java
@@ -1,26 +1,24 @@
 package week08d02;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class Lottery {
 
-    public static final int NUMBER_OF_NUMBERS = 5;
-    public static final int NUMBERS_INTERVAL = 90;
+    public static final int NUMBER_OF_NUMBERS = 7;
+    public static final int NUMBERS_INTERVAL = 35;
 
     public List<Integer> getNumbers() {
         Random random = new Random();
-        List<Integer> result = new ArrayList<>(5);
-
+        List<Integer> result = new ArrayList<>(7);
         for (int i = 0; i < NUMBER_OF_NUMBERS; i++) {
             result.add(generateNumberNotInList(result));
         }
+        Collections.sort(result);
         return result;
     }
 
         private Integer generateNumberNotInList(List<Integer> myList) {
-        Random random = new Random(10);
+        Random random = new Random();
         int number = random.nextInt(NUMBERS_INTERVAL) + 1;
         while (myList.contains(number)) {
             number = random.nextInt(NUMBERS_INTERVAL) +1;
