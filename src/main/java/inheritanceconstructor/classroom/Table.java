0e9fbe5904a48e139ceb49cc825c7dca32788044
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-02-19 10:49:45 +0100

    inheritanceconstructor.classroom commit #2

---
+++ src/main/java/inheritanceconstructor/classroom/Table.java
@@ -0,0 +1,8 @@
+package inheritanceconstructor.classroom;
+
+public class Table extends ClassRoom {
+
+    public Table(String location, int capacity, Facility facility) {
+        super(location, capacity, facility);
+    }
+}
