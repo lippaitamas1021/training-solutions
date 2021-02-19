862cce95e79ec7adac05b9cac11618e22d27412a
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-02-19 07:10:44 +0100

    exam03 Histogram commit #2

--- src/main/java/exam03/Histogram.java
+++ src/main/java/exam03/Histogram.java
@@ -2,38 +2,28 @@ package exam03;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 public class Histogram {
 
     public String createHistogram(BufferedReader reader) {
-        int counter = 0;
+        StringBuilder stringBuilder = new StringBuilder();
         try {
             String line;
             while ((line = reader.readLine()) != null) {
-                //System.out.println(line);
-                if (line.contains(String.valueOf(4))) {
-                    System.out.println("****");
-                }
-                if (line.contains(String.valueOf(3))) {
-                        System.out.println("***");
-                    }
-                if (line.contains(String.valueOf(2))) {
-                    System.out.println("**");
-            }
-                if (line.contains(String.valueOf(1))) {
-                    System.out.println("*");
-                }
+                stringBuilder.append(convertToStars(Integer.parseInt(line)));
+                stringBuilder.append("\n");
                 }
         } catch (IOException ioe) {
             throw new IllegalStateException("Failed at reading the file", ioe);
         }
-        return null;
+        return stringBuilder.toString();
     }
 
-    public static void main(String[] args) throws IOException {
-        Histogram histogram = new Histogram();
-        histogram.createHistogram((Files.newBufferedReader(Path.of("histogram.txt"))));
+    private String convertToStars(int number) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < number; i++) {
+            sb.append('*');
+        }
+        return sb.toString();
     }
 }
