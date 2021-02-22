b96cbd4e0edbc01bd4dceb4b83fa729cb7fc3660
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-02-22 11:21:54 +0100

    week16d01 VowelFilter commit

---
+++ src/main/java/week16d01/VowelFilter.java
@@ -0,0 +1,40 @@
+package week16d01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VowelFilter {
+
+    List<String> vowels = new ArrayList<>(List.of("a", "á", "e", "é", "i", "í", "o", "ó", "u", "ú", "ü", "ű"));
+
+    public String filterVowels(BufferedReader bufferedReader) throws IOException {
+        String line;
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            while ((line = bufferedReader.readLine()) != null) {
+                //System.out.println(line);
+                String[] temp = line.split("");
+                for (int i = 0; i < temp.length; i++) {
+                    if (vowels.contains(temp[i].toLowerCase())) {
+                        stringBuilder.append(temp[i]);
+                        stringBuilder.append("\n");
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new IllegalStateException("Failed at reading", e);
+        }
+        //System.out.println(stringBuilder.toString());
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = Files.newBufferedReader(Path.of("words.txt"));
+        VowelFilter vowelFilter = new VowelFilter();
+        System.out.println(vowelFilter.filterVowels(bufferedReader));
+    }
+}
