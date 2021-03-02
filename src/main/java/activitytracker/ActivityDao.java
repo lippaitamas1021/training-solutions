f06af8cebefe0ba84892b0c62d55cf68202b625f
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-03-02 17:14:23 +0100

    activitytracker Transactions commit

--- src/main/java/activitytracker/ActivityDao.java
+++ src/main/java/activitytracker/ActivityDao.java
@@ -23,44 +23,106 @@ public class ActivityDao {
             preparedStatement.setString(2, activity.getDesc());
             preparedStatement.setString(3, activity.getType().toString());
             preparedStatement.executeUpdate();
-            try (ResultSet resultSet = preparedStatement.getGeneratedKeys()) {
-                if (resultSet.next()) {
-                    long id = resultSet.getLong(1);
-                    return new Activity(id, activity.getStartTime(), activity.getDesc(), activity.getType());
-                }
-            }
+            Activity result = getIdAfterExecuted(activity, preparedStatement);
+            insertActivityTrackPoints(activity.getTrackPoints(), result.getId());
+            return result;
         } catch (SQLException se) {
             throw new IllegalStateException("Connection failed", se);
         }
-        return activity;
+    }
+
+    private Activity getIdAfterExecuted(Activity activity, PreparedStatement preparedStatement) throws SQLException {
+        try (ResultSet resultSet = preparedStatement.getGeneratedKeys()) {
+            if (resultSet.next()) {
+                long id = resultSet.getLong(1);
+                return new Activity(id, activity.getStartTime(), activity.getDesc(), activity.getType());
+            }
+        }
+        throw new IllegalStateException("Can not get keys");
+    }
+
+    private void insertActivityTrackPoints(List<TrackPoint> trackPointList, long activityId) {
+        try(Connection connection = dataSource.getConnection()) {
+            connection.setAutoCommit(false);
+            try (PreparedStatement preparedStatement = connection.prepareStatement("insert into track_points (act_time, lat, lon, activityId) values (?, ?, ?, ?)")) {
+                for (TrackPoint trackPoint : trackPointList) {
+                    if (!isValidLatLon(trackPoint.getLat(), trackPoint.getLon())) {
+                        throw new IllegalArgumentException("Invalid lat or lon");
+                    }
+                    preparedStatement.setDate(1, Date.valueOf(trackPoint.getTime()));
+                    preparedStatement.setDouble(2, trackPoint.getLat());
+                    preparedStatement.setDouble(3, trackPoint.getLon());
+                    preparedStatement.setLong(4, activityId);
+                    preparedStatement.executeUpdate();
+                }
+                connection.commit();
+            } catch (IllegalArgumentException iae) {
+                connection.rollback();
+            }
+        } catch (SQLException se) {
+            throw new IllegalStateException("Can not connect", se);
+        }
+    }
+
+    private boolean isValidLatLon(double lat, double lon) {
+        if (lat < -90 || 90 < lat) {
+            return false;
+        }
+        if (lon < -180 || 180 < lon) {
+            return false;
+        }
+        return true;
     }
 
     private List<Activity> selectActivityByPreparedStatement(PreparedStatement preparedStatement) {
         List<Activity> result = new ArrayList<>();
         try (ResultSet resultSet = preparedStatement.executeQuery()) {
-            if (resultSet.next()) {
+            while (resultSet.next()) {
                 Activity activity = new Activity(
                         resultSet.getLong("id"),
                         resultSet.getTimestamp("start_time").toLocalDateTime(),
                         resultSet.getString("activity_desc"),
                         ActivityType.valueOf(resultSet.getString("activity_type")));
                 result.add(activity);
-            } else {
-                throw new IllegalArgumentException("Activitiy not found");
             }
+            return result;
         } catch (SQLException se) {
             throw new IllegalStateException("Not found");
         }
-        return result;
     }
 
-    public List<Activity> selectActivityById(long id) {
-        try(Connection connection = dataSource.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement("select * from activities where id=?")) {
-            preparedStatement.setLong(1, id);
-            return selectActivityByPreparedStatement(preparedStatement);
+    private List<TrackPoint> selectTrackPointsByPreparedStatement(PreparedStatement preparedStatement) {
+        List<TrackPoint> result = new ArrayList<>();
+        try (ResultSet resultSet = preparedStatement.executeQuery()) {
+            while (resultSet.next()) {
+                TrackPoint trackPoint = new TrackPoint(
+                        resultSet.getLong("id"),
+                        resultSet.getDate("act_time").toLocalDate(),
+                        resultSet.getDouble("lat"),
+                        resultSet.getLong("lon"));
+                result.add(trackPoint);
+            }
+            return result;
         } catch (SQLException se) {
-            throw new IllegalStateException("Wrong input", se);
+            throw new IllegalStateException("Not found");
+        }
+    }
+
+    public Activity selectActivityById(long id) {
+        try(Connection connection = dataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("select * from activities where id=?");
+            PreparedStatement preparedStatement2 = connection.prepareStatement("select * from track_points where activityId = ?")) {
+            preparedStatement.setLong(1, id);
+            List<Activity> result = selectActivityByPreparedStatement(preparedStatement);
+            if (result.size() == 1) {
+                preparedStatement2.setLong(1, id);
+                List<TrackPoint> resultPoints = selectTrackPointsByPreparedStatement(preparedStatement2);
+                result.get(0).addTrackPoints(resultPoints);
+                return result.get(0);
+            }
+            throw new IllegalArgumentException("Not found");
+        } catch (SQLException se) {
+            throw new IllegalStateException("Can not connect", se);
         }
     }
 
@@ -117,5 +179,13 @@ public class ActivityDao {
         ActivityDao activityDao = new ActivityDao(dataSource);
         activityDao.activitiesBeforeDate(LocalDate.of(2021,3,3));
         System.out.println(activityDao.activitiesBeforeDate(LocalDate.of(2021,3,3)));
+
+        Activity activity = new Activity(LocalDateTime.of(2021,3,3,13,10),"Running", ActivityType.RUNNING);
+        activity.addTrackPoints(List.of(
+                new TrackPoint(LocalDate.of(2021,3,3), 67, 23),
+                new TrackPoint(LocalDate.of(2021,3,3), 68, 22),
+                new TrackPoint(LocalDate.of(2021,3,3), 69, 21)));
+        Activity activity2 = activityDao.insertActivity(activity);
+        System.out.println(activityDao.selectActivityById(activity2.getId()));
     }
 }
--- src/test/java/activitytracker/ActivityDaoTest.java
+++ src/test/java/activitytracker/ActivityDaoTest.java
@@ -4,16 +4,20 @@ import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mariadb.jdbc.MariaDbDataSource;
+
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ActivityDaoTest {
+
     private ActivityDao activityDao;
 
     @BeforeEach
-    void setUp() {
+    public void setUp() {
         MariaDbDataSource dataSource;
         try {
             dataSource = new MariaDbDataSource();
@@ -30,19 +34,37 @@ class ActivityDaoTest {
     }
 
     @Test
-    void insertActivityTest() {
+    public void insertActivityTest() {
         Activity activity1 = new Activity(LocalDateTime.of(2021,3,2,9,30),"Biking in Mátra", ActivityType.BIKING);
         activityDao.insertActivity(activity1);
         assertEquals(4, activityDao.selectAllActivities().size());
     }
 
     @Test
-    void selectAllActivitiesTest() {
+    public void selectAllActivitiesTest() {
         assertEquals(3, activityDao.selectAllActivities().size());
     }
 
     @Test
     public void selectBeforeDateTest() {
         System.out.println(activityDao.activitiesBeforeDate(LocalDate.of(2021,3,3)));
+    }
+
+    @Test
+    public void findByIdTest() {
+        Activity activity = new Activity(LocalDateTime.of(2021,3,2,10,0),"Biking", ActivityType.BIKING);
+        Activity result = activityDao.insertActivity(activity);
+        assertEquals("Biking", activityDao.selectActivityById(result.getId()).getDesc());
+    }
+
+    @Test
+    public void insertActivityWithTrackPointsTest() {
+        Activity activity = new Activity(LocalDateTime.of(2021,3,3,13,10),"Running", ActivityType.RUNNING);
+        activity.addTrackPoints(List.of(
+                new TrackPoint(LocalDate.of(2021,3,3), 67, 23),
+                new TrackPoint(LocalDate.of(2021,3,3), 68, 22),
+                new TrackPoint(LocalDate.of(2021,3,3), 69, 21)));
+        Activity activity2 = activityDao.insertActivity(activity);
+        System.out.println(activityDao.selectActivityById(activity2.getId()));
     }
 }
