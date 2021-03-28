e0bfb96f8e347fba0e71b63d886bbc5e3e32899d
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-03-28 10:05:32 +0200

    initializer.callhierarchy commit

---
+++ src/main/java/initializer/callhierarchy/Bird.java
@@ -0,0 +1,16 @@
+package initializer.callhierarchy;
+
+public class Bird {
+
+    static {
+        System.out.println("Bird class static initializer block, class loaded");
+    }
+
+    public Bird() {
+        System.out.println("Bird class constructor");
+    }
+
+    {
+        System.out.println("Bird class initializer block");
+    }
+}
