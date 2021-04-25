77f694bbfc05bef91a26bd2f69ece0ec9f37b31d
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-04-25 11:14:40 +0200

    vowelcount commit

---
+++ src/test/java/vowelcount/VowelsTest.java
@@ -0,0 +1,14 @@
+package vowelcount;
+
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class VowelsTest {
+
+    @Test
+    void vowelCount() {
+        int[] r = new int[]{1, 3, 3, 1, 0};
+        assertEquals(true, Arrays.equals(r, Vowels.vowelCount("i think, therefore i am")) );
+    }
+}
